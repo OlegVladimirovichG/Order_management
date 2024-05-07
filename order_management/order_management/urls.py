0944b8f762ae135cd1038a2b28db3d548a61e628
Oleg Golubev <hankat@mail.ru>
2024-05-07 15:06:42 +0300

    commit 2

--- order_management/order_management/urls.py
+++ order_management/order_management/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.order_list, name='order_list'),
     path('create/', views.create_order, name='create_order'),
     path('update/<int:pk>/', views.update_order, name='update_order'),
+    path('cancel/<int:pk>/', views.cancel_order, name='cancel_order'),
 ]
--- order_management/orders/views.py
+++ order_management/orders/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order
 from .forms import OrderForm
+
 
 def order_list(request):
     orders = Order.objects.all()
     return render(request, 'orders/order_list.html', {'orders': orders})
+
 
 def create_order(request):
     if request.method == 'POST':
@@ -16,6 +18,7 @@ def create_order(request):
         form = OrderForm()
     return render(request, 'orders/order_form.html', {'form': form})
 
+
 def update_order(request, pk):
     order = Order.objects.get(pk=pk)
     if request.method == 'POST':
@@ -26,3 +29,10 @@ def update_order(request, pk):
     else:
         form = OrderForm(instance=order)
     return render(request, 'orders/order_form.html', {'form': form})
+
+
+def cancel_order(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order.status = 'cancelled'
+    order.save()
+    return redirect('order_list')
