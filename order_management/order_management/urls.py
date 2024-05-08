2b60406f40a23c9be400e35ad873a33515d5585e
Oleg Golubev <hankat@mail.ru>
2024-05-08 08:35:49 +0300

    added delete

--- order_management/order_management/urls.py
+++ order_management/order_management/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create/', views.create_order, name='create_order'),
     path('update/<int:pk>/', views.update_order, name='update_order'),
     path('cancel/<int:pk>/', views.cancel_order, name='cancel_order'),
+    path('change_status/<int:pk>/', views.change_status, name='change_status'),
+    path('delete/<int:pk>/', views.delete_order, name='delete_order'),
 ]
